d220d294919c4ae1d08a767f830e1470135b941c
ram-arthasoft <ramchandra@arthasoftsolutions.com>
2021-12-22 10:01:14 +0545

    added datetime

--- final Project/Frames/supportingFunctions.py
+++ final Project/Frames/supportingFunctions.py
@@ -1,12 +1,12 @@
-import datetime
+from datetime import datetime
 from tkinter import messagebox
 from win32api import GetSystemMetrics, WinExec
 
 def warnUser( text):
     messagebox.showinfo("Warning", text)
 
-def getDateTime(self):
-    nw = datetime.now(self)
+def getDateTime(self=""):
+    nw = datetime.now()
     date = nw.strftime("%d/%m/%Y")
     time = nw.strftime("%H:%M")
     return (date,time)
