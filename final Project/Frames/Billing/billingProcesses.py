11037700fb63034f4499e54355bc8b72461f8f3e
ram-arthasoft <ramchandra@arthasoftsolutions.com>
2021-12-25 14:19:58 +0545

    formated

--- final Project/Frames/Billing/billingProcesses.py
+++ final Project/Frames/Billing/billingProcesses.py
@@ -2,11 +2,13 @@ from tkinter import *
 from tkinter import messagebox
 import Frames.Billing.viewProductsInBill as viewProductsInBill
 
-from config.dynamicSize import FR,WR,HR
+from config.dynamicSize import FR, WR, HR
 from Frames.supportingFunctions import warnUser
 import pymongo
-#New Add Product Funtion to add products in bill
-def billingProcess(self,viewTree):
+# New Add Product Funtion to add products in bill
+
+
+def billingProcess(self, viewTree):
     try:
         def on_closing():
             top.destroy()
@@ -31,27 +33,33 @@ def billingProcess(self,viewTree):
                     top.destroy()
                 else:
                     self.productsInBill[productToBill['Product Name']] = {}
-                    self.productsInBill[productToBill['Product Name']]['Quantity'] = requiredQuantity
-                    self.productsInBill[productToBill['Product Name']]['iid'] = productToBill['_id']
-                    self.productsInBill[productToBill['Product Name']]['Sales Price'] = salesPrice
-                    self.productsInBill[productToBill['Product Name']]['Units'] = productToBill['Units']
-                    productTotal = int(self.productsInBill[productToBill["Product Name"]]['Sales Price'])*requiredQuantity
-                    self.productsInBill[productToBill['Product Name']]['Product Total'] = productTotal
-                    viewProductsInBill.viewProductsInBill(self,viewTree)
+                    self.productsInBill[productToBill['Product Name']
+                                        ]['Quantity'] = requiredQuantity
+                    self.productsInBill[productToBill['Product Name']
+                                        ]['iid'] = productToBill['_id']
+                    self.productsInBill[productToBill['Product Name']
+                                        ]['Sales Price'] = salesPrice
+                    self.productsInBill[productToBill['Product Name']
+                                        ]['Units'] = productToBill['Units']
+                    productTotal = int(
+                        self.productsInBill[productToBill["Product Name"]]['Sales Price'])*requiredQuantity
+                    self.productsInBill[productToBill['Product Name']
+                                        ]['Product Total'] = productTotal
+                    viewProductsInBill.viewProductsInBill(self, viewTree)
                     top.destroy()
 
         # client = MongoClient("mongodb+srv://rootUser:clouddbaccess@trialdbs.i4jhu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
         # db = client.get_database('saiRecords')
         # collection = db.inventory
 
-        ##For local database storage
+        # For local database storage
 
         def validateSalesPrice(e):
             try:
                 float(askSalesPriceEntry.get())
             except ValueError:
-                askSalesPriceEntry.delete(-1,'end')
-       
+                askSalesPriceEntry.delete(-1, 'end')
+
         connection = pymongo.MongoClient("localhost", 27017)
         database = connection['saiRecords']
         collection = database['inventory']
@@ -60,7 +68,7 @@ def billingProcess(self,viewTree):
 
         productToBill = collection.find_one({'Product Name': toadd})
 
-        if (productToBill['Quantity']==0):
+        if (productToBill['Quantity'] == 0):
             raise ValueError
 
         # connection.close()
@@ -71,27 +79,33 @@ def billingProcess(self,viewTree):
         availableQuantity = Label(
             top, text="Available Quantity", padx=5, pady=5, font=('Helvetica', int(FR*15), 'bold'))
         availableQuantity.grid(row=0, column=0)
-        displayAvailableQuantity = Label(top, text=(str(productToBill['Quantity'])+" "+ str(productToBill['Units'])), padx=5, pady=5, font=('Comic Sans MS', int(FR*15), 'bold'))
+        displayAvailableQuantity = Label(top, text=(str(productToBill['Quantity'])+" " + str(
+            productToBill['Units'])), padx=5, pady=5, font=('Comic Sans MS', int(FR*15), 'bold'))
         displayAvailableQuantity.grid(row=0, column=1)
 
-        askQuantityLabel = Label(top, text="Enter Quantity", padx=5, pady=5, font=('Helvetica', int(FR*15), 'bold'))
+        askQuantityLabel = Label(top, text="Enter Quantity", padx=5, pady=5, font=(
+            'Helvetica', int(FR*15), 'bold'))
         askQuantityLabel.grid(row=1, column=0)
 
-        askQuantityEntry = Entry(top, width = int(WR*10), font=('Comic Sans MS', int(FR*15), 'bold'))
+        askQuantityEntry = Entry(top, width=int(
+            WR*10), font=('Comic Sans MS', int(FR*15), 'bold'))
         askQuantityEntry.grid(row=1, column=1)
 
-        askSalesPriceLabel = Label(top, text="Sales Price", padx=5, pady=5, font=('Helvetica', int(FR*15), 'bold'))
+        askSalesPriceLabel = Label(top, text="Sales Price", padx=5, pady=5, font=(
+            'Helvetica', int(FR*15), 'bold'))
         askSalesPriceLabel.grid(row=2, column=0)
-        askSalesPriceEntry = Entry(top, width = int(WR*10), font=('Comic Sans MS', int(FR*15), 'bold'))
-        askSalesPriceEntry.insert(0,productToBill['Sales Price'])
+        askSalesPriceEntry = Entry(top, width=int(
+            WR*10), font=('Comic Sans MS', int(FR*15), 'bold'))
+        askSalesPriceEntry.insert(0, productToBill['Sales Price'])
         askSalesPriceEntry.grid(row=2, column=1)
-        askSalesPriceEntry.bind("<KeyRelease>",validateSalesPrice)
+        askSalesPriceEntry.bind("<KeyRelease>", validateSalesPrice)
 
-        askQuantityEntry.focus_set()
+        askQuantityEntry.focus()
         top.bind('<Return>', displayToBillView)
 
-        okBtn = Button(top, text="Sell", padx=5,pady=10, width = int(WR*8),font=('Georgia', int(FR*10),'bold'), command=displayToBillView)
-        okBtn.grid(row=3, column=0,columnspan=2)
+        okBtn = Button(top, text="Sell", padx=5, pady=10, width=int(
+            WR*8), font=('Georgia', int(FR*10), 'bold'), command=displayToBillView)
+        okBtn.grid(row=3, column=0, columnspan=2)
         top.protocol("WM_DELETE_WINDOW", on_closing)
 
     except TypeError:
@@ -99,24 +113,27 @@ def billingProcess(self,viewTree):
         self.executing = False
         # top.destroy()
     except ValueError:
-        self.executin = False
-        messagebox.showerror('Invalid Request', 'The selected product seems Out of Stock. Try adding the product in the inventory')
+        self.executing = False
+        messagebox.showerror(
+            'Invalid Request', 'The selected product seems Out of Stock. Try adding the product in the inventory')
+
 
 def getConnect(self):
     # client = MongoClient("mongodb+srv://rootUser:clouddbaccess@trialdbs.i4jhu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
     # db = client.get_database('saiRecords')
     # collection = db.inventory
-    connection = pymongo.MongoClient('localhost',27017)
+    connection = pymongo.MongoClient('localhost', 27017)
     database = connection['saiRecords']
     collection = database['inventory']
     return collection
 
-def billingEditProcess(self,viewTree):
+
+def billingEditProcess(self, viewTree):
     def validateQuantity(e):
         try:
-            value=int(quantityEntry.get())
+            value = int(quantityEntry.get())
         except ValueError:
-            quantityEntry.delete(-1,'end')
+            quantityEntry.delete(-1, 'end')
 
     def applyEdits(event=''):
 
@@ -136,9 +153,9 @@ def billingEditProcess(self,viewTree):
                 self.productsInBill[productname]['Product Total'] = newProductCost
 
                 viewTree.set(iidEdit, column='Quantity',
-                                value=newValue)
+                             value=newValue)
                 self.billingAmountLabel.config(text=self.billingTotalAmount)
-                viewProductsInBill.viewProductsInBill(self,viewTree)
+                viewProductsInBill.viewProductsInBill(self, viewTree)
                 top.destroy()
         except ValueError:
             messagebox.showerror(
@@ -156,7 +173,7 @@ def billingEditProcess(self,viewTree):
         getProduct = (viewTree.item(iidEdit, 'values'))[0]
         collection = getConnect(self)
         rslt = collection.find_one({'Product Name': getProduct}, {
-                                    'Quantity': 1, '_id': 0})
+            'Quantity': 1, '_id': 0})
         avalQuantity = float(rslt['Quantity'])
         availableQuantity = Label(
             top, text="Available Quantity", padx=5, pady=5, font=('Helvetica', int(FR*15), 'bold'))
@@ -165,15 +182,17 @@ def billingEditProcess(self,viewTree):
             top, text=avalQuantity, padx=5, pady=5, font=('Comic Sans MS', int(FR*15), 'bold'))
         displayAvailableQuantity.grid(row=0, column=1)
 
-        quantityLabel = Label(top, text="Enter New Quantity", padx=5, pady=5, font=('Helvetica', int(FR*15), 'bold'))
+        quantityLabel = Label(top, text="Enter New Quantity", padx=5, pady=5, font=(
+            'Helvetica', int(FR*15), 'bold'))
         quantityLabel.grid(row=1, column=0)
 
-
-        quantityEntry = Entry(top, width = int(WR*10),  font=('Comic Sans MS', int(FR*15), 'bold'))
+        quantityEntry = Entry(top, width=int(
+            WR*10),  font=('Comic Sans MS', int(FR*15), 'bold'))
         quantityEntry.grid(row=1, column=1)
         quantityEntry.focus()
-        quantityEntry.bind('<KeyRelease>',validateQuantity)
+        quantityEntry.bind('<KeyRelease>', validateQuantity)
         quantityEntry.bind('<Return>', applyEdits)
 
-        editbtn = Button(top, text="Change", command=applyEdits, font=('Georgia',int(FR*10),'bold'))
-        editbtn.grid(row=2, column=0, pady =12)
+        editbtn = Button(top, text="Change", command=applyEdits,
+                         font=('Georgia', int(FR*10), 'bold'))
+        editbtn.grid(row=2, column=0, pady=12)
