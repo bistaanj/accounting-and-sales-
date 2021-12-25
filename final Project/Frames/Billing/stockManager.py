2efcbda22e18bff41eeb7bbf67cfcafb3a43f1c5
bistaanj <bistaanj@gmail.com>
2021-12-25 19:12:11 +0530

    Added 'outStock' collection and sales recorded in collection

--- final Project/Frames/Billing/stockManager.py
+++ final Project/Frames/Billing/stockManager.py
@@ -1,11 +1,15 @@
 import pymongo
 from bson.objectid import ObjectId
+from datetime import datetime
 connection = pymongo.MongoClient("localhost", 27017)
 database = connection['saiRecords']
 collection = database['presentStock']
 
 
 def manageStock(id, quantity):
+    connection = pymongo.MongoClient("localhost", 27017)
+    database = connection['saiRecords']
+    collection = database['presentStock']
     a = quantity
     ref_id = id
     q=collection.find_one({'_id':ObjectId(ref_id)}) 
@@ -15,11 +19,16 @@ def manageStock(id, quantity):
     oc = q['Stock'][-1]['CP']
     print(" oq, oc")
     print(oq, oc )
+    nw = datetime.now()
+    id = nw.strftime("%d%m%Y-%H%M%S")
+    rawdata=[]
     # collection.update_one({'_id': ObjectId(ref_id)}, {'$push':{'Stock':{'Quantity':600,'CP':50}}})
     while(a != 0):
-        # a=a-1
-        # print(a)
+        innerDict={}
         if (oq< a):
+            innerDict['Quantity']= oq
+            innerDict['CP']= oc
+            rawdata.append(innerDict)
             a = a - oq
             collection.update_one({'_id':ObjectId(ref_id)}, {'$pop': {'Stock': 1 } } )
             q=collection.find_one({'_id':ObjectId(ref_id)}) 
@@ -27,15 +36,16 @@ def manageStock(id, quantity):
             oc = q['Stock'][-1]['CP']
         else:
             newValue = oq - a
+            innerDict['Quantity']= newValue
+            innerDict['CP']= oc
+            rawdata.append(innerDict)
             a=0
             collection.update_one({'_id':ObjectId(ref_id)}, {'$pop': {'Stock': 1 } } )
             collection.update_one({'_id': ObjectId(ref_id)}, {'$push':{'Stock':{'Quantity':newValue,'CP':oc}}})
+    collection = database['outStock']
+    collection.update_one({'_id':ObjectId(ref_id)},{id:rawdata})
     
-    q=collection.find_one({'_id':ObjectId(ref_id)}) 
-    for x in q['Stock']:
-        print(x)
-
-
+    
         
 # a=(q.Stock)
 # print(a[0])
