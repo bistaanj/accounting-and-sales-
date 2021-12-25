5528ad696cba26070e5a772e819e35c885391506
bistaanj <bistaanj@gmail.com>
2021-12-25 17:07:12 +0530

    completed

--- final Project/Frames/Billing/stockManager.py
+++ final Project/Frames/Billing/stockManager.py
@@ -4,15 +4,21 @@ connection = pymongo.MongoClient("localhost", 27017)
 database = connection['saiRecords']
 collection = database['presentStock']
 
+
 def manageStock(id, quantity):
     a = quantity
     ref_id = id
     q=collection.find_one({'_id':ObjectId(ref_id)}) 
+    for x in q['Stock']:
+        print(x)
     oq =(q['Stock'][-1]['Quantity'])
     oc = q['Stock'][-1]['CP']
     print(" oq, oc")
     print(oq, oc )
+    # collection.update_one({'_id': ObjectId(ref_id)}, {'$push':{'Stock':{'Quantity':600,'CP':50}}})
     while(a != 0):
+        # a=a-1
+        # print(a)
         if (oq< a):
             a = a - oq
             collection.update_one({'_id':ObjectId(ref_id)}, {'$pop': {'Stock': 1 } } )
@@ -24,6 +30,12 @@ def manageStock(id, quantity):
             a=0
             collection.update_one({'_id':ObjectId(ref_id)}, {'$pop': {'Stock': 1 } } )
             collection.update_one({'_id': ObjectId(ref_id)}, {'$push':{'Stock':{'Quantity':newValue,'CP':oc}}})
-            
-    q=collection.find_one({'_id':ObjectId(ref_id)}) 
     
+    q=collection.find_one({'_id':ObjectId(ref_id)}) 
+    for x in q['Stock']:
+        print(x)
+
+
+        
+# a=(q.Stock)
+# print(a[0])
